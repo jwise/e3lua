e44959bf5e610d6f0144703e4f7728e6476ebf8c
Joshua Wise <root@nyus.joshuawise.com>
2009-08-18 00:56:07 -0400

    Ext3BG: Initialize 'fs' at Ext3BG:new time

--- Ext3BG.lua
+++ Ext3BG.lua
@@ -23,14 +23,16 @@ function Ext3BG:new(o)
         return o
 end
 
-function Ext3BG:read(disk, sb, bg)
-        local sectors_per_block = bit.lshift(1024 / disk.BYTES_PER_SECTOR, sb.log_block_size)
-        local sector = (sb.block_group_nr * sb.blocks_per_group + 1) * sectors_per_block
+function Ext3BG:read(bg)
+	local fs = self.fs
+	
+        local sectors_per_block = fs.block_size / fs.disk.BYTES_PER_SECTOR
+        local sector = (fs.sb.block_group_nr * fs.sb.blocks_per_group + 1) * sectors_per_block
         -- 32: sizeof(ext3 block descriptor); there is probably a nicer way
         -- to get this value, but i don't know what it is. halp, lunary. --car
-        sector = sector + (bg * 32) / disk.BYTES_PER_SECTOR
+        sector = sector + (bg * 32) / self.disk.BYTES_PER_SECTOR
 
-        local data = disk:read_sector(sector)
+        local data = fs.disk:read_sector(sector):sub((bg * 32) % self.disk.BYTES_PER_SECTOR + 1)
         local desc = serial.read.struct(serial.buffer(data), self.format)
 
         -- copied from Ext3SB.lua ... is this necessary? i do not know.
